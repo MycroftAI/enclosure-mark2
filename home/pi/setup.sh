422d11f2f1483900c4dfd8f56c21b836192c2290
David Wagner <david@wagnerkc.com>
2019-07-16 09:24:06 -0500

    Prefer remove and repopulate over chown'ing existing files

--- home/pi/setup.sh
+++ home/pi/setup.sh
@@ -30,9 +30,11 @@ sudo rm -rf /opt/venvs
 
 # Correct permissions from Mark 1 (which used the 'mycroft' user to run)
 sudo chown -R pi:pi /var/log/mycroft
-sudo chmod 666 /var/log/mycroft/*
+rm /var/log/mycroft/*
+#sudo chmod 666 /var/log/mycroft/*
 sudo chown -R pi:pi /opt/mycroft
-sudo chown -R pi:pi /tmp/*
+rm -rf /tmp/*
+#sudo chown -R pi:pi /tmp/*
 
 # Display Setup
 sudo echo "# Mark 2 Pi Display Settings" | sudo tee -a /boot/config.txt    
