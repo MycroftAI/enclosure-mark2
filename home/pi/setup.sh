0cf6cb5053dfa5cd4c45896b105f615ed0003cf8
David Wagner <david@wagnerkc.com>
2019-07-16 10:23:33 -0500

    -y to autoremove

--- home/pi/setup.sh
+++ home/pi/setup.sh
@@ -92,7 +92,7 @@ IS_TRAVIS=true bash dev_setup.sh 2>&1 | tee ../build.log
 ~/mycroft-core/bin/mycroft-msm install skill-mark-2
 
 # Clean apt cache
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 
 # Development
 sudo touch /boot/ssh
